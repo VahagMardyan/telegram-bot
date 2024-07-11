3c98e69e47039b08388f14a4bd286c7829fd63e0
VahagMardyan <mardyan2003@gmail.com>
2024-07-11 07:13:44 +0400

    Deleted tokens

--- bot.py
+++ bot.py
@@ -185,9 +185,9 @@ dialog.list = []
 dialog.count = 0
 dialog.user = {}
 
-chatgpt = ChatGptService(token="gpt:6iWpGTIVtrvZB0KTFlGqJFkblB3Tjikl0aobFUM6zUmVCkXU")
+chatgpt = ChatGptService(token="YOUR_ChatGPT_TOKEN")
 
-app = ApplicationBuilder().token("7499864908:AAGcEySPUw4WRmEAJiDJC8V96It5WxULQLA").build()
+app = ApplicationBuilder().token("YOUR_TELEGRAM_BOT_TOKEN").build()
 app.add_handler(CommandHandler("start",start_bot))
 app.add_handler(CommandHandler("gpt",gpt_bot))
 app.add_handler(CommandHandler("date",date))
